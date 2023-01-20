20fb9ebc69e74ebdf9e948097fbb9697c5e7dcb2
BiGameRad8 <95191528+BiGameRad8@users.noreply.github.com>
2023-01-20 09:05:06 -0500

    Add files via upload
    dev console by snowlord7
    inspect element like console

---
+++ devconsole.js
@@ -0,0 +1,1 @@
+javascript:(function(){(function() {/*found by BigameRad8 on github*/var x = document.createElement("script");x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js";x.onload = alert("Loaded Developer Console!");document.head.appendChild(x);})()})()
